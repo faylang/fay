65e499a84f987de756a22476f182d831ff39c0a6
Chris Done <chrisdone@gmail.com>
2012-07-27 20:24:48 +0200

    Add warning in the build script.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,19 @@
+module Main where
+
+import Distribution.PackageDescription
 import Distribution.Simple
-main = defaultMain
+
+main = defaultMainWithHooks simpleUserHooks
+       { preConf = \_args _flags -> do putStrLn reminder
+                                       return emptyHookedBuildInfo
+       }
+
+reminder =
+  "                                                                               \n\
+  \- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+  \                                                                               \n\
+  \  REMEMBER: This compiler is in flux, supercalifragelistic style. You should   \n\
+  \            read the CHANGELOG for this release as the changes probably        \n\
+  \            affect you.                                                        \n\
+  \                                                                               \n\
+  \- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"

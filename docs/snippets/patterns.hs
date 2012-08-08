746653fc4d39585d80d52324a88c1a909037dc95
Chris Done <chrisdone@gmail.com>
2012-08-09 00:37:42 +0200

    Fix patterns snippet.

--- docs/snippets/patterns.hs
+++ docs/snippets/patterns.hs
@@ -1,3 +1,9 @@
+data Math = Math
+  { root :: Double -> Double
+  , square :: Double -> Double
+  , cube :: Double -> Double
+  }
+
 race (winner,runners) = print (winner,runners)
 
 first (x:xs) = x

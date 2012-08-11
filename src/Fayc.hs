ebd1e16d7697654a7c0b461309058799cd49894c
Ben Ford <ben.fordnz@gmail.com>
2012-08-11 09:40:32 +0100

    Add --pretty and stdin support to fayc
    Example usage:
    
    fay git:(fayc)$ echo "main = print \"one\"" | ./cabal-dev/bin/fayc -
    var main = new $(function() {
        return _(print)(Fay$$list("one"));
    });
    
    fay git:(fayc)$ ./cabal-dev/bin/fayc --help
    fayc - the fay compiler
    fayc [options] [- | files]
    fayc - takes input on stdin (implies pretty)
    fayc [files] processes each .hs file
    
    Help Options:
      -h, --help                  Show option summary.
      --help-all                  Show all help options.
    
    Application Options:
      --tco
      --autorun
      --inline-force
      --flatten-apps
      --export-builtins
      -s, --stdout                Output to stdout
      -p, --pretty                Run javascript through js-beautify

--- src/Fayc.hs
+++ src/Fayc.hs
@@ -3,16 +3,26 @@
 
 module Main where
 
-import           Language.Fay.Compiler
-import           Language.Fay.Types
+import Language.Fay (compileModule, compileViaStr)
+import Language.Fay.Compiler
+import Language.Fay.Types
 
-import           Control.Monad
-import           Data.Default
-import           Data.List
-import           Options
-import           System.Environment
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Default
+import Data.List
+import Options
+import System.Environment
+import System.Exit
+import System.IO
+import System.Process
 
 defineOptions "FayCompilerOptions" $ do
+  boolOption "optTCO" "tco" False ""
+  boolOption "optAutoRun" "autorun" False ""
+  boolOption "optInlineForce" "inline-force" False ""
+  boolOption "optFlattenApps" "flatten-apps" False ""
+  boolOption "optExportBuiltins" "export-builtins" True ""
   option "optStdout" (\o -> o
         { optionLongFlags = ["stdout"]
         , optionShortFlags = ['s']
@@ -20,47 +30,83 @@ defineOptions "FayCompilerOptions" $ do
         , optionType = optionTypeBool
         , optionDescription = "Output to stdout"
                             })
-  option "optQuiet" (\o -> o
-        { optionLongFlags = ["quiet"]
-        , optionShortFlags = ['q']
+  option "optPretty" (\o -> o
+        { optionLongFlags = ["pretty"]
+        , optionShortFlags = ['p']
         , optionDefault = "false"
         , optionType = optionTypeBool
-        , optionDescription = "Whether to be quiet."
-        })
+        , optionDescription = "Run javascript through js-beautify"
+                            })
+        
+helpTxt :: String
+helpTxt = unlines [
+          "fayc -- The fay compiler from (a proper subset of) Haskell to Javascript"
+        , "USAGE"
+        , "  fayc [OPTIONS] [- | <hs-file>...]"
+        , "fayc - takes input on stdin and pretty prints to stdout. Requires js-beautify"
+        , "fayc <hs-file>... processes each .hs file"
+        ]
 
--- | Main entry point.
 main :: IO ()
-main = runCommand $ \opts args -> do
-  if optStdout opts
-     then print "stdout"
-     else print "done"
-  print $ show args
--- main = do
---   args <- getArgs
---   let files = filter (not . isPrefixOf "-") args
---       opts = map (drop 1) $ filter (isPrefixOf "-") args
---   if (elem "help" opts) || null files
---     then putStrLn helpText
---     else forM_ files $ \file -> do
---       compileFromTo def { configTCO = elem "tco" opts
---                         , configInlineForce = elem "inline-force" opts
---                         , configFlattenApps = elem "flatten-apps" opts
---                         , configExportBuiltins = not (elem "no-export-builtins" opts)
---                         }
---         (elem "autorun" opts)
---         file
---         (toJsName file)
+main = runCommandHelp helpTxt $ \opts files -> do
+    let config = def { configTCO = optTCO opts
+                     , configInlineForce = optInlineForce opts
+                     , configFlattenApps = optFlattenApps opts
+                     , configExportBuiltins = optExportBuiltins opts
+                     }
+    let compile' = compile config (optAutoRun opts)
+    case files of
+         ["-"] -> do
+               putStrLn =<< prettyCompile config compileModule =<< hGetContents stdin
+                     
+         otherwise -> forM_ files $ \file -> do
+             compile'
+                     (readFile file)
+                     (writer opts file)
+    where
+        --format :: (Options opts) => opts -> (String -> IO String)
+        format opts = case optPretty opts of
+                       True -> beautify
+                       False -> return . id
+        writer opts file js = do
+                        js' <- format opts js
+                        case optStdout opts of
+                            True -> hPutStr stdout js'
+                            False -> writeFile (toJsName file) js'
+                  
+               
+runCommandHelp :: (MonadIO m, Options opts) => String -> (opts -> [String] -> m a) -> m a
+runCommandHelp help io = do
+	argv <- liftIO System.Environment.getArgs
+	let parsed = parseOptions argv
+	case parsedOptions parsed of
+		Just opts -> io opts (parsedArguments parsed)
+		Nothing -> liftIO $ do
+                        putStrLn help
+                        case parsedError parsed of
+			    Just err -> do
+			    	hPutStrLn stderr (parsedHelp parsed)
+			    	hPutStrLn stderr err
+			    	exitFailure
+			    Nothing -> do
+			    	hPutStr stdout (parsedHelp parsed)
+			    	exitSuccess
 
-
--- helpText = unlines
---   ["fay -- compiler from (a proper subset of) Haskell to JavaScript"
---   ,""
---   ,"USAGE"
---   ,"  fay [OPTIONS] <hs-input-file> ... "
---   ,""
---   ,"OPTIONS"
---   ,"  -autorun       automatically call main in generated JavaScript"
---   ,"  -inline-force  inline forcing, adds some speed for numbers, blows up code a bit"
---   ,"  -flatten-apps  flatten function application, can be more readable,"
---   ,"                 no noticeable speed difference"
---   ]
+prettyCompile :: (Show from,Show to,CompilesTo from to)
+              => CompileConfig
+              -> (from -> Compile to)
+              -> String
+              -> IO (String)
+prettyCompile config with from = do
+  result <- compileViaStr config with from
+  case result of
+    Left err -> return $ show err
+    Right (ok,_) -> beautify ok
+                      
+beautify :: String -> IO (String)
+beautify js = do
+    (Just hin, Just hout, _, _) <-
+       createProcess (proc "js-beautify" ["-"]){ std_in = CreatePipe, std_out = CreatePipe }
+    hPutStrLn hin js
+    hClose hin
+    hGetContents hout

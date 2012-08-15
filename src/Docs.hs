178c6d4dc8030ac71b1aba489bde1b13f51502cd
Chris Done <chrisdone@gmail.com>
2012-08-15 11:36:31 +0200

    Update github link in docs.

--- src/Docs.hs
+++ src/Docs.hs
@@ -93,7 +93,7 @@ thebody now analytics examples = do
   preEscapedToMarkup analytics
 
 fork =
-  a ! href "https://github.com/chrisdone/fay" $
+  a ! href "https://github.com/faylang/fay" $
     img ! style "position: absolute; top: 0; right: 0; border: 0;"
         ! src "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
         ! alt "Fork me on GitHub"

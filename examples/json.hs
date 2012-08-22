b74a44593c7ea1e3f481f1cc0b0653a9c1eb0d09
Kyle Hanson <hanooter@gmail.com>
2012-08-22 10:39:47 -0500

    Added JSON
    Added Example of to and from JSON

---
+++ examples/json.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Console where
+
+import           Language.Fay.FFI
+import           Language.Fay.Prelude
+
+data MyData = MyData  { xVar :: Int, yVar :: Int }
+instance Foreign MyData
+
+myData :: MyData
+myData = MyData { yVar = 3, xVar = 9 }
+
+myFunction :: MyData -> MyData -> Int
+myFunction a b = (xVar a) + (xVar b)
+
+main = do
+	jsonSerialized <- toJSON myData
+	jsonDeserialized <- toMyData jsonSerialized
+	fromStringData <- toMyData "{\"xVar\":3,\"yVar\":-1}"
+	printInt $ myFunction jsonDeserialized fromStringData
+
+-- | Print using console.log.
+print :: String -> Fay ()
+print = ffi "console.log(%1)"
+
+printBool :: Bool -> Fay ()
+printBool = ffi "console.log(%1)"
+
+printInt :: Int -> Fay ()
+printInt = ffi "console.log(%1)"
+
+toMyData :: String -> Fay MyData
+toMyData = ffi "JSON.parse(%1)"
+
+toJSON :: MyData -> Fay String
+toJSON = ffi "JSON.stringify(%1)"

0e3668f058117e3ebfac53b62f3d192552577da2
Chris Done <chrisdone@gmail.com>
2012-09-03 00:33:17 +0200

    Added calculator example.

---
+++ examples/calc.hs
@@ -0,0 +1,126 @@
+-- | Calculator based on http://trelford.com/PitCalculatorApp.htm
+--
+-- Compile with
+-- $ fay --autorun -p --html-wrapper --html-js-lib jquery.min.js examples/calc.hs
+--
+
+{-# OPTIONS -fno-warn-orphans -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
+{-# LANGUAGE EmptyDataDecls    #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Calc (main) where
+
+import Language.Fay.FFI
+import Language.Fay.Prelude
+
+main :: Fay ()
+main = do
+  display <- select "<input type='text' value='' style='text-align:right'>"
+  operation <- newRef Nothing :: Fay (Ref (Maybe (Double -> Double)))
+  appendMore <- newRef False
+
+  let buttons =
+        [[enter 7, enter 8, enter 9, ("/",operator (\x y -> x / y))]
+        ,[enter 4, enter 5, enter 6, ("*",operator (\x y -> x * y))]
+        ,[enter 1, enter 2, enter 3, ("-",operator (\x y -> x - y))]
+        ,[("C",clear), enter 0, ("=",calculate), ("+",operator (\x y -> x + y))]]
+      enter n = (show n,do current <- getVal display
+                           addit <- readRef appendMore
+                           if addit
+                              then do setVal (current ++ show n) display
+                                      return ()
+                              else do setVal (show n) display
+                                      writeRef appendMore True)
+      getInput = getVal display >>= return . parseDouble 10
+      operator op = do
+        calculate
+        writeRef appendMore False
+        num <- getInput
+        writeRef operation (Just (op num))
+      clear = do
+        setVal "0" display
+        calculate
+      calculate = do
+        op <- readRef operation
+        case op of
+          Nothing -> return ()
+          Just maths -> do
+            num <- getInput
+            setVal (show (maths num)) display
+            return ()
+        writeRef operation Nothing
+        writeRef appendMore False
+
+  ready $ do
+    body <- select "body"
+    table <- select "<table></table>" & appendTo body
+    dtr <- select "<tr></tr>" & appendTo table
+    select "<td colspan='4'></td>" & appendTo dtr & append display
+    forM_ buttons $ \row -> do
+      tr <- select "<tr></tr>" & appendTo table
+      forM_ row $ \(text,action) -> do
+        td <- select "<td></td>" & appendTo tr
+        select "<input type='button' value='' style='width:32px'>"
+              & setVal text
+              & appendTo td
+              & onClick (do action; return False)
+
+--------------------------------------------------------------------------------
+-- JQuery bindings
+
+data JQuery
+instance Foreign JQuery
+instance Show JQuery
+
+data Element
+instance Foreign Element
+
+-- | Nicer/easier binding for >>=.
+(&) :: Fay a -> (a -> Fay b) -> Fay b
+x & y = x >>= y
+infixl 1 &
+
+getVal :: JQuery -> Fay String
+getVal = ffi "%1['val']()"
+
+setVal :: String -> JQuery -> Fay JQuery
+setVal = ffi "%2['val'](%1)"
+
+select :: String -> Fay JQuery
+select = ffi "window['jQuery'](%1)"
+
+ready :: Fay () -> Fay ()
+ready = ffi "window['jQuery'](%1)"
+
+append :: JQuery -> JQuery -> Fay JQuery
+append = ffi "%2['append'](%1)"
+
+appendTo :: JQuery -> JQuery -> Fay JQuery
+appendTo = ffi "%2['appendTo'](%1)"
+
+onClick :: Fay Bool -> JQuery -> Fay JQuery
+onClick = ffi "%2['click'](%1)"
+
+--------------------------------------------------------------------------------
+-- Utilities
+
+instance (Foreign a) => Foreign (Maybe a)
+
+parseDouble :: Int -> String -> Double
+parseDouble = ffi "parseFloat(%2,%1)"
+
+--------------------------------------------------------------------------------
+-- Refs
+
+data Ref a
+instance Show (Ref a)
+instance Foreign a => Foreign (Ref a)
+
+newRef :: Foreign a => a -> Fay (Ref a)
+newRef = ffi "new Fay$$Ref(%1)"
+
+writeRef :: Foreign a => Ref a -> a -> Fay ()
+writeRef = ffi "Fay$$writeRef(%1,%2)"
+
+readRef :: Foreign a => Ref a -> Fay a
+readRef = ffi "Fay$$readRef(%1)"

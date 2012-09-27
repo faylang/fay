6f53e01675ef795e2b991651dba68be831d755f5
Ben Ford <ben.fordnz@gmail.com>
2012-09-27 06:51:31 +0100

    Add support for unpacking nested tuples and test
    This now also seems to be doing the right thing with thunks

--- tests/BindTuple.hs
+++ tests/BindTuple.hs
@@ -9,15 +9,24 @@ unPackCase tup = case tup of (a, b) -> a
 
 unPackLet tup = let (a, b) = tup in b
 
+undefined :: Foreign a => Fay a
+undefined = ffi "undefined"
+
+unPackNested =
+  let ((a, b), c) = (("Hi", "From"), "Nested")
+  in concat $ intersperse " " [a, b, c]
+
 unPackFnApplication str =
     let (a, b) = f str
     in a ++ " " ++  b
     where f str = ("Hi", str)
+
 main = do
   let tup = ("Hi", "There")
   print $ unPackCase tup
   print $ unPackLet tup
   print $ unPackFnApplication "Again"
+  print unPackNested
 
 
 print :: String -> Fay ()

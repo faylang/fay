52be5498872773bf7d5d3be445a37db4229d462b
Ben Ford <ben.fordnz@gmail.com>
2012-10-04 06:02:17 +0100

    Test case for operators failing to be lazy enough

--- tests/LazyOperators.hs
+++ tests/LazyOperators.hs
@@ -6,10 +6,26 @@ import           Language.Fay.FFI
 import           Language.Fay.Prelude
 
 main :: Fay ()
-main = print testFn
+main = do
+     let lazy = fnLoop lazyAdder 3
+         strict = fnLoop strictAdder 3
+     print lazy
+     print strict
+     print dontEval
 
+dontEval = let f a b = snd (a/b,10) in f 1 0 -- undefined undefined
 
-testFn = let f a b = snd (a/b,10) in f 1 0 -- undefined undefined
+plus a b = a + b
+
+lazyAdder :: (Double, Double) -> (Double, Double)
+lazyAdder tup  = let (c, d) = tup in (c `plus` d, c)
+
+strictAdder :: (Double, Double) -> (Double, Double)
+strictAdder tup  = let (c, d) = tup in (c + d, c)
+
+-- ArrowLoop instance for (->)
+fnLoop :: ((t1, t2) -> (t, t2)) -> t1 -> t
+fnLoop f b = let (c,d) = f (b,d) in c
 
 print :: Double -> Fay ()
 print = ffi "console.log(%1)"

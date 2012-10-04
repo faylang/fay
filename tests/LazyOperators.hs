f3a280bea60409ddfae515ad6c3541036b94cf6e
Ben Ford <ben.fordnz@gmail.com>
2012-10-04 06:02:17 +0100

    Test case for operators failing to be lazy enough

---
+++ tests/LazyOperators.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module WhereBind where
+
+import           Language.Fay.FFI
+import           Language.Fay.Prelude
+
+main :: Fay ()
+main = do
+     let lazy = fnLoop lazyAdder 3
+         strict = fnLoop strictAdder 3
+     print lazy
+     print strict
+
+plus a b = a + b
+
+lazyAdder :: (Double, Double) -> (Double, Double)
+lazyAdder tup  = let (c, d) = tup in (c `plus` d, c)
+
+strictAdder :: (Double, Double) -> (Double, Double)
+strictAdder tup  = let (c, d) = tup in (c + d, c)
+
+-- ArrowLoop instance for (->)
+fnLoop :: ((t1, t2) -> (t, t2)) -> t1 -> t
+fnLoop f b = let (c,d) = f (b,d) in c
+
+print :: Double -> Fay ()
+print = ffi "console.log(%1)"

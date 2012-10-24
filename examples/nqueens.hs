448706673ebf9c81c722709e53867fbbbd507c97
Chris Done <chrisdone@gmail.com>
2012-10-24 23:24:40 +0200

    Added nqueens to examples.

---
+++ examples/nqueens.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- !!! count the number of solutions to the "n queens" problem.
+
+module NQueens where
+
+import Language.Fay.Prelude
+import Language.Fay.FFI
+
+main :: Fay ()
+main = benchmark $ print (nsoln 11)
+
+print :: Int -> Fay ()
+print = ffi "console.log(%1)"
+
+listLength :: [a] -> Int -> Int
+listLength [] acc = acc
+listLength (_:l) acc = listLength l (1 + acc)
+
+listFilter :: (a -> Bool) -> [a] -> [a]
+listFilter f [] = []
+listFilter f  (a : l)
+    | f a       = a : (listFilter f l)
+    | otherwise = listFilter f l
+
+nsoln :: Int -> Int
+nsoln nq = listLength (gen nq) 0
+ where
+    safe :: Int -> Int -> [Int] -> Bool
+    safe x d []        = True
+    safe x d ( q : l) = (x /= q) && (x /= (q+d)) && (x /= (q-d)) && safe x (d+1) l
+
+    gen :: Int -> [[Int]]
+    gen 0 =  [] : []
+    gen n = concat $ map (\bs -> map (\q ->  q : bs) $ listFilter (\q -> safe q 1 bs) ([1..nq])) (gen (n-1))
+
+benchmark m = do
+  start <- getSeconds
+  m
+  end <- getSeconds
+  printS (show (end-start) ++ "ms")
+
+printS :: String -> Fay ()
+printS = ffi "console.log(%1)"
+
+getSeconds :: Fay Double
+getSeconds = ffi "new Date()"

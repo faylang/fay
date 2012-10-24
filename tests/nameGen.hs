a0151c5d02312f440cd8c65e8235877c0c719f2a
Chris Done <chrisdone@gmail.com>
2012-10-24 22:27:56 +0200

    Add test case for nameGen (closes #121).

---
+++ tests/nameGen.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module NameGen where
+
+import           Language.Fay.FFI
+import           Language.Fay.Prelude
+
+data SomeRec = SomeRec { a :: Integer, b :: Integer } | Y | X
+
+
+main = do
+    -- https://github.com/faylang/fay/issues/121
+    print_str $ case Y of
+                    SomeRec _ _ -> "Bad"
+                    Y -> "OK."
+
+    let t = Y
+    print_str $ case t of
+                    SomeRec _ _ -> "Bad"
+                    Y -> "OK."
+
+
+print_str :: String -> Fay ()
+print_str = ffi "console.log(%1)"

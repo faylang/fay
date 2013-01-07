c42645904c9395db50fd06ee4dcabaa9c3f3bfd2
Michael Snoyman <michael@snoyman.com>
2013-01-07 19:43:48 +0200

    Basic CPP support (#136)

--- src/Language/Fay/Compiler.hs
+++ src/Language/Fay/Compiler.hs
@@ -85,7 +85,38 @@ compileToAst filepath state with from =
 
 -- | Parse some Fay code.
 parseFay :: Parseable ast => FilePath -> String -> ParseResult ast
-parseFay filepath = parseWithMode parseMode { parseFilename = filepath }
+parseFay filepath = parseWithMode parseMode { parseFilename = filepath } . applyCPP
+
+-- | Apply incredibly simplistic CPP handling. It only recognizes the following:
+--
+-- > #if FAY
+-- > #ifdef FAY
+-- > #ifndef FAY
+-- > #else
+-- > #endif
+--
+-- Note that this implementation replaces all removed lines with blanks, so
+-- that line numbers remain accurate.
+applyCPP :: String -> String
+applyCPP =
+    unlines . loop NoCPP . lines
+  where
+    loop _ [] = []
+    loop state ("#if FAY":rest) = "" : loop (CPPIf True state) rest
+    loop state ("#ifdef FAY":rest) = "" : loop (CPPIf True state) rest
+    loop state ("#ifndef FAY":rest) = "" : loop (CPPIf False state) rest
+    loop (CPPIf b oldState) ("#else":rest) = "" : loop (CPPElse (not b) oldState) rest
+    loop (CPPIf _ oldState) ("#endif":rest) = "" : loop oldState rest
+    loop (CPPElse _ oldState) ("#endif":rest) = "" : loop oldState rest
+    loop state (x:rest) = (if toInclude state then x else "") : loop state rest
+
+    toInclude NoCPP = True
+    toInclude (CPPIf x _) = x
+    toInclude (CPPElse x _) = x
+
+data CPPState = NoCPP
+              | CPPIf Bool CPPState
+              | CPPElse Bool CPPState
 
 -- | The parse mode for Fay.
 parseMode :: ParseMode

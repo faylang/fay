bf639621f88d070e5911591b6fb0f4b4764025b6
Chris Done <chrisdone@gmail.com>
2013-01-29 13:35:37 +0100

    Update console example.

--- examples/console.hs
+++ examples/console.hs
@@ -1,6 +1,12 @@
 module Console (main) where
 
-main = putStrLn "Hello, World!"
+import Prelude
 
-putStrLn :: String -> Fay ()
-putStrLn = ffi "console.log(%1)"
+main = putStrLn (showInt (fib 10))
+
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+showInt :: Int -> String
+showInt = ffi "%1+''"

0bfc57a494b0958ed004ee64fcf0471eee86aa52
Adam Bergmark <adam@edea.se>
2013-05-08 23:36:18 +0200

    Add -base-path flag to fay-tests

--- src/Tests.hs
+++ src/Tests.hs
@@ -30,24 +30,25 @@ main :: IO ()
 main = do
   sandbox <- fmap (lookup "HASKELL_PACKAGE_SANDBOX") getEnvironment
   (packageConf,args) <- fmap (prefixed (=="-package-conf")) getArgs
-  compiler <- makeCompilerTests (packageConf <|> sandbox)
+  let (basePath,args') = prefixed (=="-base-path") args
+  compiler <- makeCompilerTests (packageConf <|> sandbox) basePath
   defaultMainWithArgs [Api.tests, Cmd.tests, compiler, C.tests]
-                      args
+                      args'
 
 -- | Extract the element prefixed by the given element in the list.
 prefixed :: (a -> Bool) -> [a] -> (Maybe a,[a])
 prefixed f (break f -> (x,y)) = (listToMaybe (drop 1 y),x ++ drop 2 y)
 
 -- | Make the case-by-case unit tests.
-makeCompilerTests :: Maybe FilePath -> IO Test
-makeCompilerTests packageConf = do
+makeCompilerTests :: Maybe FilePath -> Maybe FilePath -> IO Test
+makeCompilerTests packageConf basePath = do
   files <- fmap (map ("tests" </>) . sort . filter (isSuffixOf ".hs")) $ getDirectoryContents "tests"
   return $ testGroup "Tests" $ flip map files $ \file -> testCase file $ do
-    testFile packageConf False file
-    testFile packageConf True file
+    testFile packageConf basePath False file
+    testFile packageConf basePath True file
 
-testFile :: Maybe FilePath -> Bool -> String -> IO ()
-testFile packageConf opt file = do
+testFile :: Maybe FilePath -> Maybe FilePath -> Bool -> String -> IO ()
+testFile packageConf basePath opt file = do
   let root = (reverse . drop 1 . dropWhile (/='.') . reverse) file
       out = toJsName file
       config =
@@ -55,6 +56,7 @@ testFile packageConf opt file = do
           def { configOptimize = opt
               , configTypecheck = False
               , configPackageConf = packageConf
+              , configBasePath = basePath
               }
   outExists <- doesFileExist root
   let partialName = root ++ "_partial"

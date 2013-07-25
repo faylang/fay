50b3dda4303846f96f93fd3420c148a48c726484
Adam Bergmark <adam@edea.se>
2013-07-25 14:15:28 +0200

    Remove unused QName.unQual'

--- src/Fay/Compiler/QName.hs
+++ src/Fay/Compiler/QName.hs
@@ -11,11 +11,6 @@ unQual (Qual _ n) = n
 unQual (UnQual n) = n
 unQual Special{} = error "unQual Special{}"
 
-unQual' :: QName -> QName
-unQual' (Qual _ n) = UnQual n
-unQual' u@UnQual{} = u
-unQual' Special{}  = error "unQual' Special{}"
-
 changeModule :: ModuleName -> QName -> QName
 changeModule m (Qual _ n) = Qual m n
 changeModule m (UnQual n) = Qual m n

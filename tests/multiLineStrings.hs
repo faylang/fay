dd16b6a4cb140d2522f9c8a6344eb9239bf8f07c
Anupam Jain <ajnsit@gmail.com>
2013-08-07 18:25:14 +0530

    Added dependency on cpphs. Added tests for multiline strings.

---
+++ tests/multiLineStrings.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Main where
+
+import Prelude
+import FFI
+
+printAll :: [String] -> Fay ()
+printAll = ffi "\
+  \(function(xs) {\
+  \  var prn = function(x) {\
+  \    console.log(x);\
+  \  };\
+  \  for(var i=0; i<xs.length; i++) {\
+  \    prn(xs[i]);\
+  \  }\
+  \})(%1)"
+
+main :: Fay ()
+main = printAll ["a", "b", "c"]
+

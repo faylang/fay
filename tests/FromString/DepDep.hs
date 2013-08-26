d9eb425341039f27601036281d8529041a7703ad
Sebastiaan Visser <code@fvisser.nl>
2013-08-26 16:23:54 +0200

    Implemented overloadable string literals using an in-scope fromString.
    Uses a combination of OverloadedStrings and RebindableSyntax.

---
+++ tests/FromString/DepDep.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings, RebindableSyntax #-}
+module FromString.DepDep where
+
+import Prelude
+import FromString.FayText
+
+myText :: Text
+myText = "This is also not a String"
+

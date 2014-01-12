8f2de40c0714eb6d73873064a2eb82930a0c5a4c
Adam Bergmark <adam@bergmark.nl>
2014-01-12 23:45:24 +0100

    More newtype ffi tests

--- tests/newtype.hs
+++ tests/newtype.hs
@@ -22,13 +22,30 @@ newtype Baz = Baz { unwrapBaz :: Foo }
 getBaz :: Fay Baz
 getBaz = ffi "{ instance: 'Bar', bar: 1 }"
 
+getBazExpr = ffi "{ instance: 'Bar', bar : 2 }" :: Fay Baz
+
+getBazExpr' = f (Baz (Bar 2)) (ffi "{ instance: 'Bar', bar : 3 }" :: Baz)
+f :: Baz -> Baz -> Baz
+f (Baz x) (Baz y) = Baz (Bar (bar x + bar y))
+
+
 main = do
     print x
     print y
     print yInt
     print (Baz (Bar 1))
+
     baz <- getBaz
     print baz
-    case baz of
-      Baz (Bar i) -> print i
+    case baz of Baz (Bar i) -> print i
     print (bar $ unwrapBaz baz)
+
+    bazExpr <- getBazExpr
+    print bazExpr
+    case bazExpr of Baz (Bar i) -> print i
+    print (bar $ unwrapBaz bazExpr)
+
+    let bazExpr' = getBazExpr'
+    print bazExpr'
+    case bazExpr' of Baz (Bar i) -> print i
+    print (bar $ unwrapBaz bazExpr')
--- tests/newtype.res
+++ tests/newtype.res
@@ -5,3 +5,9 @@
 { instance: 'Bar', bar: 1 }
 1
 1
+{ instance: 'Bar', bar: 2 }
+2
+2
+{ instance: 'Bar', bar: 5 }
+5
+5

8d39b8a2cf3c11e22b5c6464ca50d9c6ac6389eb
Adam Bergmark <adam@bergmark.nl>
2014-10-25 23:40:27 +0200

    fay-test: Add flag for randomly running some tests, for travis

--- src/tests/Tests.hs
+++ src/tests/Tests.hs
@@ -15,9 +15,12 @@ import qualified Test.Convert         as Convert
 import qualified Test.Desugar         as Desugar
 import           Test.Util
 
+import           Data.Set             (Set)
+import qualified Data.Set             as S
 import           System.Directory
 import           System.Environment
 import           System.FilePath
+import           System.Random
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
@@ -25,10 +28,11 @@ import           Test.Tasty.HUnit
 main :: IO ()
 main = do
   sandbox <- fmap (lookup "HASKELL_PACKAGE_SANDBOX") getEnvironment
-  (packageConf,args) <- fmap (prefixed (=="-package-conf")) getArgs
-  let (basePath,args') = prefixed (=="-base-path") args
-  (runtime,codegen) <- makeCompilerTests (packageConf <|> sandbox) basePath
-  withArgs args' $ defaultMain $ testGroup "Fay"
+  (packageConf,args)     <- prefixed (== "-package-conf") <$> getArgs
+  let (basePath,args')    = prefixed (== "-base-path"   ) args
+  let (testCount, args'') = first (readMay =<<) $ prefixed (== "-random") args'
+  (runtime,codegen) <- makeCompilerTests (packageConf <|> sandbox) basePath testCount
+  withArgs args'' $ defaultMain $ testGroup "Fay"
     [ Desugar.tests
     , Convert.tests
     , codegen
@@ -42,10 +46,11 @@ prefixed :: (a -> Bool) -> [a] -> (Maybe a,[a])
 prefixed f (break f -> (x,y)) = (listToMaybe (drop 1 y),x ++ drop 2 y)
 
 -- | Make the case-by-case unit tests.
-makeCompilerTests :: Maybe FilePath -> Maybe FilePath -> IO (TestTree,TestTree)
-makeCompilerTests packageConf basePath = do
-  runtimeFiles <- runtimeTestFiles
-  codegenFiles <- codegenTestFiles
+makeCompilerTests :: Maybe FilePath -> Maybe FilePath -> Maybe Int -> IO (TestTree,TestTree)
+makeCompilerTests packageConf basePath rand = do
+  runtimeFiles' <- runtimeTestFiles
+  runtimeFiles  <- maybe (return runtimeFiles') (randomize runtimeFiles') rand
+  codegenFiles  <- codegenTestFiles
   return
     (makeTestGroup "Runtime tests"
                    runtimeFiles
@@ -67,6 +72,17 @@ makeCompilerTests packageConf basePath = do
       getRecursiveContents "tests"
     nonRuntime x =
       any (`isInfixOf` x) ["/Compile/","/regressions/","/codegen/"]
+    randomize :: [String] -> Int -> IO [String]
+    randomize l i = do
+      is <- randomizeAux S.empty i (0,length l - 1)
+      return . map (l !!) $ S.toList is
+    randomizeAux :: Set Int -> Int -> (Int,Int) -> IO (Set Int)
+    randomizeAux s count b = do
+      i <- randomRIO b
+      let s' = S.insert i s
+      if S.size s' == count
+        then return s'
+        else randomizeAux (S.insert i s) count b
 
 testFile :: Maybe FilePath -> Maybe FilePath -> Bool -> String -> IO ()
 testFile packageConf basePath opt file = do

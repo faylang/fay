c35b57fbd7a0f96e00e155733e08a6cf8c4fb64f
Renzo Carbonara <gnuk0001@gmail.com>
2017-06-11 14:34:39 +0200

    use other nixpkgs version

--- release.nix
+++ release.nix
@@ -1,8 +1,8 @@
 { nixpkgs ? (import <nixpkgs> {}).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "5bafc71b61301bac2233beee29985185ec95aa50"; # release-17.03
-    sha256 = "1zh1dqm5pbfhrv6ww1dxlr9q2s5k3ixj35x1ghm4w3h7vh9alwqn"; }
+    rev = "d3b66f9b946db3c612fb6f4c28efd9e46f924e97"; # release-17.03
+    sha256 = "07x3h4ld4mzsbz6znx0wbx38kywf1bqnrdv7p41rg96dw2pr6lfk"; }
 }:
 
 
@@ -12,8 +12,10 @@ let
     fay = self.callPackage (import ./pkg.nix) {};
   };
 
-  ghc802 = pkgs.haskell.packages.ghc802.override { inherit packageSetConfig; };
   ghc7103 = pkgs.haskell.packages.ghc7103.override { inherit packageSetConfig; };
+  ghc802 = pkgs.haskell.packages.ghc802.override { inherit packageSetConfig; };
 
-
-in pkgs.runCommand "dummy" { buildInputs = [ ghc802.fay ghc7103.fay ]; } ""
+in {
+  fay_ghc7103 = ghc802.fay;
+  fay_ghc802 = ghc802.fay;
+}
